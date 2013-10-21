fb390ae79bc2c39506907097f30351e261ed6e22
XavierGomez <axgp_007@hotmail.com>
2013-10-20 23:44:15 -0500

    set y get
    implementacion de los metodos de skill y employment

--- code/Node/src/java/model/entities/Employment.java
+++ code/Node/src/java/model/entities/Employment.java
@@ -1,6 +1,26 @@
 package model.entities;
 
-public class Employment 
+public class Employment implements java.io.Serializable
 {
+    private int employmetId;
+    private String name;
     
+    public Employment(){
+    }
+    
+    public void setemploymentId(int employmentId){
+        this.employmetId = employmentId;
+    }
+    
+    public int getemployment(){
+        return this.employmetId;
+    }
+    
+    public void setname(String name){
+        this.name = name;
+    }
+    
+    public String getname(){
+        return this.name;
+    }
 }
--- code/Node/src/java/model/entities/Skill.java
+++ code/Node/src/java/model/entities/Skill.java
@@ -1,6 +1,26 @@
 package model.entities;
 
-public class Skill 
+public class Skill implements java.io.Serializable
 {
+    private int skillId;
+    private String name;
     
+    public Skill(){
+    }
+    
+    public void setskillId(int skillId){
+        this.skillId= skillId;
+    }
+    
+    public int getskillId(){
+        return this.skillId;
+    }
+    
+    public void setname(String name){
+        this.name= name;
+    }
+    
+    public String getname(){
+        return this.name;
+    }
 }
